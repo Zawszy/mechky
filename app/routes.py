29cfd850535da94d657ebbb5677df419b6376214
Zawszy <zachyi.lim@dreamerman.net>
2020-05-04 17:26:59 +1000

    rough website design

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,10 @@ from app import app
 from flask import render_template, request
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
     return render_template('index.html')
+
+@app.route('/keyboards')
+def keyboards():
+    return render_template('keyboards.html')
